084da2566727ca4229faeaaa2c705faa30d1c46b
Andres20055 <147523978+Andres20055@users.noreply.github.com>
2023-11-01 17:56:40 -0500

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,24 @@
+def add(numero1,numero2):
+  resultado= numero1 + numero2
+  print(f'{numero1} + {numero2} is equal to {resultado}')
+  return resultado
+def resta(numero1,numero2):
+  resultado= numero1 - numero2
+  print(f'{numero1} - {numero2} is equal to {resultado}')
+  return resultado
+def multiplicacion (numero1,numero2):
+  resultado= numero1 * numero2
+  print(f'{numero1} * {numero2} is equal to {resultado}')
+  return resultado
+def division(numero1,numero2):
+  resultado= numero1/numero2
+  print(f'{numero1} / {numero2} is equal to {resultado}')
+  return resultado
+def exponente(numero1,numero2):
+  resultado= pow(numero1,numero2)
+  print(f'{numero1} pow{numero2} is equal to {resultado}')
+  return resultado
+def modulo(numero1,numero2):
+  result= numero1 % numero2
+  print(f'{numero1} % {numero2} is equal to {resultado}')
+  return resultado
